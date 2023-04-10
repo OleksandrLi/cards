32f208fa350e9c457e2c96625065b7b5bae1dbac
OleksandrLi <lipstort@gmail.com>
2023-04-10 18:56:23 +0300

    init project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return <div>123</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
