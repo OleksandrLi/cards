ae7921ce5658bde2862d9dd6a9aee7ce8a433c60
OleksandrLi <lipstort@gmail.com>
2023-04-10 21:35:14 +0300

    add data

---
+++ src/data/lessons/times.ts
@@ -0,0 +1,25 @@
+export const timesForLessons = [
+  "Present Simple",
+  "Past Simple",
+  "Future Simple",
+  "Present Perfect",
+  "Past Perfect",
+  "Future Simple",
+  "Present Continuous",
+  "Past Continuous",
+  "Future Continuous",
+];
+
+export const questionsLessons = [
+  "General question",
+  "Tag question",
+  "Alternative question",
+  "Special question",
+  "Question to subject",
+];
+
+export const specialQuestions = ["What", "When", "Why", "What", "When"];
+
+// Додати через чекбокси які часи використовуютсья для уроку
+
+// Додати вибір негативного речення та питання. До специального питання додати з яким словом
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App: React.FC = () => {
-  return <div>123</div>;
+  return <div>Nearly [ˈnɪəli] Около</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
